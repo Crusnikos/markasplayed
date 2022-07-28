508fadc42d72cb574ed04b298b3403b8797ef53d
Michal Kubrak <fenrir188@gmail.com>
2022-07-28 21:32:13 +0200

    test(api,ui): test user included

---
+++ api/MarkAsPlayed.Api/Migrations/1659035930_test_user.sql
@@ -0,0 +1,14 @@
+﻿insert
+into author (
+    firebase_id,
+    name,
+    description_pl,
+    description_en
+)
+values
+(
+    '2bCXfO6Pj8Xl9eNJ4GGHe8CX14d2',
+    'Testowy użytkownik',
+    'Krótki opis testowego użytkownika',
+    'Test user short description'
+);
