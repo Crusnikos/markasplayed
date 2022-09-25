9ed0fa7c70e6b5c4e54faff954b8d9ef03736329
crusnikos <michalkubrak.90@gmail.com>
2022-09-25 23:55:46 +0200

    feat(ui): new footer design

--- ui/src/footer/index.tsx
+++ ui/src/footer/index.tsx
@@ -1,58 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Divider, Grid, Typography } from "@mui/material";
+import i18next from "i18next";
 import { makeStyles } from "tss-react/mui";
-import FooterItem from "./FooterItem";
+import { FooterItem, FooterItemWithText } from "./FooterItem";
 
 const useStyles = makeStyles()((theme) => ({
   footer: {
+    paddingBottom: theme.spacing(2),
     backgroundColor: theme.palette.primary.main,
   },
   footerContent: {
+    color: theme.palette.common.white,
     margin: "auto",
     maxWidth: "1200px",
-    padding: "6px",
+    padding: theme.spacing(1.5),
   },
-  list: {
-    marginBlockStart: "0",
-    marginBlockEnd: "0",
-    overflow: "hidden",
+  text: {
+    fontSize: 14,
+    marginBottom: theme.spacing(1.5),
+    marginRight: theme.spacing(2),
+  },
+  divider: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    width: "100%",
+    backgroundColor: theme.palette.common.white,
   },
 }));
 
-const data = [
-  {
-    icon: "icons8-mail-96.png",
-    text: "footer.author",
-    link: "mailto:michalkubrak.90@gmail.com",
-  },
-  {
-    icon: "icons8-attach-gif-96.png",
-    text: "footer.icons",
-    link: "https://icons8.com/",
-  },
-  {
-    icon: "icons8-github-96.png",
-    text: "footer.git",
-    link: "https://github.com/Crusnikos/markasplayed",
-  },
-];
-
 export default function Footer(): JSX.Element {
   const { classes } = useStyles();
+  const [data, setData] = useState<
+    [{ icon: string; link: string }] | undefined
+  >(undefined);
+
+  useEffect(() => {
+    async function fetchLocalSocialLinks() {
+      await fetch("data/socialLinks.json")
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result.data);
+        });
+    }
+
+    void fetchLocalSocialLinks();
+  }, []);
+
+  function createAboutSection(): JSX.Element[] {
+    const array = [];
+
+    for (let i = 1; i < 4; i++) {
+      array.push(
+        <Typography className={classes.text} variant="body2" key={i}>
+          {i18next.t(`footer.about.p${i}`)}
+        </Typography>
+      );
+    }
+
+    return array;
+  }
 
   return (
     <footer className={classes.footer}>
-      <div className={classes.footerContent}>
-        <ul className={classes.list}>
-          {data.map((item) => (
-            <FooterItem
-              key={item.icon}
-              icon={item.icon}
-              text={item.text}
-              link={item.link}
-            />
-          ))}
-        </ul>
-      </div>
+      <Grid container className={classes.footerContent}>
+        <Grid item lg={8} md={8}>
+          <Typography variant="h6">
+            {i18next.t("footer.header.about")}
+          </Typography>
+          {createAboutSection()}
+        </Grid>
+        <Grid item lg={4} md={4}>
+          <Typography variant="h6">
+            {i18next.t("footer.header.social")}
+          </Typography>
+          {data &&
+            data.map((item) => (
+              <FooterItem key={item.icon} icon={item.icon} link={item.link} />
+            ))}
+        </Grid>
+        <Grid container item direction="row" alignItems="center">
+          <Divider className={classes.divider} />
+        </Grid>
+        <Grid>
+          <FooterItemWithText
+            icon={i18next.t("footer.icons.icon")}
+            text={i18next.t("footer.icons.text")}
+            link={i18next.t("footer.icons.link")}
+          />
+        </Grid>
+      </Grid>
     </footer>
   );
 }
