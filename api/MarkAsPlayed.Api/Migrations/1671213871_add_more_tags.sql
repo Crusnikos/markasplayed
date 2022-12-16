b7b912e42aeb9f6f1371f84dd8911fc48233a745
Crusnikos <michalkubrak.90@gmail.com>
2022-12-16 19:52:43 +0100

    feat(api,ui): more tags included

---
+++ api/MarkAsPlayed.Api/Migrations/1671213871_add_more_tags.sql
@@ -0,0 +1,50 @@
+﻿--E stands for entertainment
+--G stands for genre
+--M stands for mode
+--P stands for pegi
+--T stands for type
+
+insert
+into tag (
+    name,
+    group_name
+)
+values
+(
+    'story', 'E'
+);
+
+insert
+into tag (
+    name,
+    group_name
+)
+values
+(
+    'sandbox', 'T'
+),
+(
+    'shooter', 'T'
+),
+(
+    'cardGame', 'T'
+),
+(
+    'diceGame', 'T'
+);
+
+insert
+into tag (
+    name,
+    group_name
+)
+values
+(
+    'mystery', 'G'
+),
+(
+    'romance', 'G'
+),
+(
+    'western', 'G'
+);
